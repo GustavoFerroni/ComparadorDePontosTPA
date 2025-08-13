be37e281a59e32d0b43385a90e5e828929f32396
GustavoFerroni <gustavoffelipe13012010@gmail.com>
2025-08-13 13:59:50 -0300

    ComparadorDePontos.java

--- ComparadorDePontos.java
+++ ComparadorDePontos.java
@@ -14,15 +14,18 @@ public class ComparadorDePontos {
     public static void main(String[] args) {
         
         //Leitura de quantos pontos cada jogador tem
+        //Pede pro usuario os números e faz o estudo dos números digitados
         String input1 = JOptionPane.showInputDialog("Digite a pontuação do Jogador 1:");
         String input2 = JOptionPane.showInputDialog("Digite a pontuação do Jogador 2:");
         String resultado = "";
         
         //Conversão para inteiros
+        //converte os números para variaveis
         int pontos1 = Integer.parseInt(input1);
         int pontos2 = Integer.parseInt(input2);
         
         //Quem fez mais pontos?
+        //Compara qual das duas pontuações é a maior
         if (pontos1 > pontos2) {
             resultado += "Jogador 1 fez MAIS pontos que o Jogador 2.\n";
         } else {
@@ -30,20 +33,23 @@ public class ComparadorDePontos {
         }
 
         //Quem fez menos pontos?
+        //Compara qual das duas pontuações é a menor
         if (pontos1 < pontos2) {
             resultado += "Jogador 1 fez MENOS pontos que o Jogador 2.\n";
         } else {
             resultado += "Jogador 1 NÃO fez menos pontos que o Jogador 2.\n";
         }
 
-        //Pontuações iguais?
+        //Pontuações iguais
+        //Aponta se as pontuações são iguais ou não
         if (pontos1 == pontos2) {
             resultado += "Ambos os jogadores têm a MESMA pontuação.\n";
         } else {
             resultado += "As pontuações são DIFERENTES.\n";
         }
 
-        //Diferente
+        //Diferentes
+        //Aponta se as pontuações são diferentes ou não
         if (pontos1 != pontos2) {
             resultado += "Jogadores têm pontuações DIFERENTES.\n";
         } else {
@@ -51,6 +57,7 @@ public class ComparadorDePontos {
         }
 
         //Maior ou igual
+        //Aponta qual a pontuação maior ou igual
         if (pontos1 >= pontos2) {
             resultado += "Jogador 1 fez MAIS ou IGUAL pontos que o Jogador 2.\n";
         } else {
@@ -58,12 +65,14 @@ public class ComparadorDePontos {
         }
 
         //Menor ou igual
+        //Aponta qual a pontuação é menor ou igual
         if (pontos1 <= pontos2) {
             resultado += "Jogador 1 fez MENOS ou IGUAL pontos que o Jogador 2.\n";
         } else {
             resultado += "Jogador 1 fez MAIS pontos que o Jogador 2.\n";
         }
         //Mostra o resultado final
+        //Exibe o resultados de todas as comparações na tela 
         JOptionPane.showMessageDialog(null, resultado);
     }
 }
